ccc29379c23211c8c2176698f57c4f76cac4b1cc
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-15 14:39:09 -0500

    updated

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to ShoppingList
+// so there's some data to look at
+ShoppingList.create('beans', true);
+ShoppingList.create('tomatoes', false);
+ShoppingList.create('peppers', false);
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
